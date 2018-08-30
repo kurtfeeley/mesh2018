71af9fe415fe1c6a9219e794e9fa40378a4d84ea
Feeley, Kurt <Kurt.Feeley@tylertech.com>
2018-08-30 08:48:26 -0400

    Added Swagger Doc

--- Mesh2018/Mesh2018/Startup.cs
+++ Mesh2018/Mesh2018/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Mesh2018
 {
@@ -26,6 +27,11 @@ namespace Mesh2018
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +48,8 @@ namespace Mesh2018
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+
         }
     }
 }
