d43daae2be00b5761ba9d01dd19bae53179785b3
Feeley, Kurt <Kurt.Feeley@tylertech.com>
2018-08-30 14:18:30 -0400

    Added Ping Controller

---
+++ Mesh2018/Mesh2018/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mesh2018.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Ping Successful");
+        }
+    }
+}
