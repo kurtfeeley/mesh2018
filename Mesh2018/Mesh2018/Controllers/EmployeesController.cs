2fbb98e6486c25fb20ab12b8f4e95abc077b495e
Feeley, Kurt <Kurt.Feeley@tylertech.com>
2018-08-30 13:18:06 -0400

    Added Employee Model

---
+++ Mesh2018/Mesh2018/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Mesh2018.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mesh2018.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // GET: api/Employee
+        [HttpGet]
+        [Route("{id}")]
+        public Employee Get(int id)
+        {
+            return new Employee { Id = 1, FirstName ="Kurt", LastName="Feeley", Address = "1 Main St.",  City="Indianapolis", State="IN", Zip="46210" };
+        }
+        
+    }
+}
---
+++ Mesh2018/Mesh2018/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mesh2018.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+    }
+}
